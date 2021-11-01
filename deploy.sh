71b3249ab1d82b3fa968ab7d96f52d2f6f546aa7
Sultan Shaikh <sultan.shaikh@hotmail.co.uk>
2021-11-01 17:39:03 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 docker pull $dockerUsername/todo-app-prod:latest
 docker tag $dockerUsername/todo-app-prod:latest registry.heroku.com/ss-todo-app/web
 echo "$HEROKU_API_KEY" | docker login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com
-docker push registry.heroku.com/ss-todo-app/web
 heroku container:login
+docker push registry.heroku.com/ss-todo-app/web
 heroku container:release web --app ss-todo-app
 
