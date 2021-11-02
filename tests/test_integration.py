e3e515623ca5953aeb8cdf62352bd5c2545eca2a
Sultan Shaikh <sultan.shaikh@hotmail.co.uk>
2021-11-02 11:11:17 +0000

    Updated

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -13,10 +13,8 @@ def client():
     # Use our test integration config instead of the 'real' version
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
-    storeConString = os.environ['MONGO_CONNECTION_STRING']
-    os.environ['MONGO_CONNECTION_STRING'] = "mongodb://admin:xyz@server.example.com/todo_app?retryWrites=true&w=majority"
+    #os.environ['MONGO_CONNECTION_STRING'] = "mongodb://admin:xyz@server.example.com/todo_app?retryWrites=true&w=majority"
     test_app = app.create_app()
-    os.environ['MONGO_CONNECTION_STRING'] = storeConString
     with test_app.test_client() as client:
         yield client
 
@@ -32,5 +30,4 @@ def test_index_page(client):
     assert response.status_code == 200
     assert 'Task8' in response.data.decode()
 
-os.environ['MONGO_CONNECTION_STRING'] = storeConString
-print(os.environ['MONGO_CONNECTION_STRING'])
+
