d55287d963c45be7f02f15f4ca275c96563076ea
Sultan Shaikh <sultan.shaikh@hotmail.co.uk>
2021-11-09 03:53:22 +0000

    u

--- terraform_env/dev/main.tf
+++ terraform_env/dev/main.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "OpenCohort1_SultanShaikh_ProjectExercise"
     storage_account_name = "tfstate007ss"
     container_name       = "tfstate007ss"
-    key                  = "terraform.tfstate"
+    key                  = "terraform_test.tfstate"
   }
 }
 provider "azurerm" {
